1c3ce47cee8aa3fe8e32773db528c4ce0218b0d0
ryo-i <iidazz0109@gmail.com>
2022-09-25 12:16:33 +0900

    勢力、人物の項目を追加 #3

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 ## 進捗（コミット）
 
 - タグの和暦、西暦、地域を優先度順で表示 [#3](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/3)
-- 地域などの項目名を整理 [#2](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2)
+- 勢力、人物の項目を追加 [#2](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2)
 
 ## 詳細（ブログ）
 
--- pages/about.tsx
+++ pages/about.tsx
@@ -59,7 +59,7 @@ function About() {
                 <h2>進捗</h2>
                 <ul>
                     <li>タグの和暦、西暦、地域を優先度順で表示 <a href="https://github.com/ryo-i/dai-sengoku-nenpyo/issues/3" target="_blank">#3</a></li>
-                    <li>地域などの項目名を整理 <a href="https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2" target="_blank">#2</a></li>
+                    <li>勢力、人物の項目を追加 <a href="https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2" target="_blank">#2</a></li>
                 </ul>
             </section>
             <section>
