2def47decb033352d53359f19ff55ad8a251faa0
ryo-i <iidazz0109@gmail.com>
2022-09-27 07:03:15 +0900

    年代、年間行を追加 #2

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 ## 進捗（コミット）
 
-- 勢力、人物の項目を追加 [#2](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2)
+- 年代、年間行を追加 [#2](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2)
 - タグclass名などを整理 [#3](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/3)
 
 ## 詳細（ブログ）
--- pages/about.tsx
+++ pages/about.tsx
@@ -58,7 +58,7 @@ function About() {
             <section>
                 <h2>進捗</h2>
                 <ul>
-                    <li>勢力、人物の項目を追加 <a href="https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2" target="_blank">#2</a></li>
+                    <li>年代、年間行を追加 <a href="https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2" target="_blank">#2</a></li>
                     <li>タグclass名などを整理 <a href="https://github.com/ryo-i/dai-sengoku-nenpyo/issues/3" target="_blank">#3</a></li>
                 </ul>
             </section>
