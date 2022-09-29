9570b9fc14b10e52ba5c0cc2337b90f3ec68e559
ryo-i <iidazz0109@gmail.com>
2022-09-30 06:41:56 +0900

    APIレスポンス、年月日のズレを修正2 #3

--- modules/api/getKeyNumber.ts
+++ modules/api/getKeyNumber.ts
@@ -4,31 +4,31 @@ const getKeyNumber = (data, keyNumbers) => {
     if (data[i] === 'adAge') {
       keyNumbers.adAge = i;
     } else if (data[i] === 'adYear') {
-      keyNumbers.adYearUnit = i;
-    } else if (data[i] === 'adYearUnit') {
       keyNumbers.adYear = i;
+    } else if (data[i] === 'adYearUnit') {
+      keyNumbers.adYearUnit = i;
     } else if (data[i] === 'adMonth') {
-      keyNumbers.adMonthUnit = i;
-    } else if (data[i] === 'adMonthUnit') {
       keyNumbers.adMonth = i;
+    } else if (data[i] === 'adMonthUnit') {
+      keyNumbers.adMonthUnit = i;
     } else if (data[i] === 'adDay') {
-      keyNumbers.adDayUnit = i;
-    } else if (data[i] === 'adDayUnit') {
       keyNumbers.adDay = i;
+    } else if (data[i] === 'adDayUnit') {
+      keyNumbers.adDayUnit = i;
     } else if (data[i] === 'waGengo') {
       keyNumbers.waGengo = i;
     } else if (data[i] === 'waYear') {
-      keyNumbers.waYearUnit = i;
-    } else if (data[i] === 'waYearUnit') {
       keyNumbers.waYear = i;
+    } else if (data[i] === 'waYearUnit') {
+      keyNumbers.waYearUnit = i;
     } else if (data[i] === 'waMonth') {
-      keyNumbers.waMonthUnit = i;
-    } else if (data[i] === 'waMonthUnit') {
       keyNumbers.waMonth = i;
+    } else if (data[i] === 'waMonthUnit') {
+      keyNumbers.waMonthUnit = i;
     } else if (data[i] === 'waDay') {
-      keyNumbers.waDayUnit = i;
-    } else if (data[i] === 'waDayUnit') {
       keyNumbers.waDay = i;
+    } else if (data[i] === 'waDayUnit') {
+      keyNumbers.waDayUnit = i;
     } else if (data[i] === 'category') {
       keyNumbers.category = i;
     } else if (data[i] === 'title') {
