ef1b42786c219dc0858db8505b0c53e9900420db
ryo-i <iidazz0109@gmail.com>
2022-10-02 20:34:48 +0900

    ページ内容をいったん非表示 #3

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 ## 進捗（コミット）
 
 - 年代、年間のテキスト一部削除 [#2](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2)
-- エラー修正テスト4 [#3](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/3)
+- ページ内容をいったん非表示 [#3](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/3)
 
 ## 詳細（ブログ）
 
--- pages/event/[title].tsx
+++ pages/event/[title].tsx
@@ -8,7 +8,19 @@ import Data from '../../data/data.json';
 
 
 // Component
-const Track = ({ eventInfo }) => {
+const Track = () => {
+    return (
+        <>
+        <Header />
+        <main>
+            <h1>てすと</h1>
+        </main>
+        <Footer />
+        </>
+    );
+}
+
+/* const Track = ({ eventInfo }) => {
     const [trackNumber, setTrackNumber] = useState(eventInfo.id);
     const [trackName, setTrackName] = useState(eventInfo.track);
 
@@ -35,23 +47,22 @@ const Track = ({ eventInfo }) => {
         <Footer />
         </>
     );
-}
+} */
 
 
 // Get Path
-export async function getStaticPaths() {
-   /* const res = await fetch(`https://dai-sengoku-nenpyo.vercel.app/api/nenpyo/nenpyolist`);
+/* export async function getStaticPaths({ params }) {
+    const res = await fetch(`https://dai-sengoku-nenpyo.vercel.app/api/nenpyo/nenpyolist`);
     const event = await res.json();
     const paths = event.trackList.map((data) => `/event/${data.title}`);
     console.log('event', event);
     console.log('paths', paths);
-    return { paths, fallback: false }; */
-}
+    return { paths, fallback: false };
+} */
 
 
 // Get TrackInfo
-// export async function getStaticProps({ params }) {
-export async function getStaticProps() {
+/* export async function getStaticProps({ params }) {
     // const title = params.event;
     const title = '1390年代：明徳2年(1392)〜応永6年(1399)';
     const res = await fetch(`https://dai-sengoku-nenpyo.vercel.app/api/nenpyo/event/${title}`);
@@ -59,6 +70,6 @@ export async function getStaticProps() {
     console.log('title', title);
     console.log('eventInfo', eventInfo);
     return { props: { eventInfo } };
-}
+} */
 
 export default Track;
