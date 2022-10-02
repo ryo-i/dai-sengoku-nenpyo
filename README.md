88a9bc2566e5d3267e2ff2d4fe8d339e1a9147cb
ryo-i <iidazz0109@gmail.com>
2022-10-02 22:06:01 +0900

    API無効化 #3

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 ## 進捗（コミット）
 
 - 年代、年間のテキスト一部削除 [#2](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2)
-- titleAPI設定テスト [#3](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/3)
+- API無効化 [#3](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/3)
 
 ## 詳細（ブログ）
 
--- pages/event/[title].tsx
+++ pages/event/[title].tsx
@@ -51,24 +51,24 @@ const Track = () => {
 
 
 // Get Path
-export async function getStaticPaths() {
+/* export async function getStaticPaths() {
     const res = await fetch(`https://dai-sengoku-nenpyo.vercel.app/api/nenpyo/nenpyolist`);
     const event = await res.json();
     const paths = event.trackList.map((data) => `/event/${data.title}`);
     // console.log('event', event);
     // console.log('paths', paths);
     return { paths, fallback: false };
-}
+} */
 
 
 // Get TrackInfo
-export async function getStaticProps({ params }) {
+/* export async function getStaticProps({ params }) {
     const title = params.title;
     const res = await fetch(`https://dai-sengoku-nenpyo.vercel.app/api/nenpyo/event/${title}`);
     const eventInfo = await res.json();
     console.log('title', title);
     console.log('eventInfo', eventInfo);
     return { props: { eventInfo } };
-}
+} */
 
 export default Track;
