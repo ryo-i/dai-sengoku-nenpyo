53e9310d215b475b2d53671df3535aca802762e9
ryo-i <iidazz0109@gmail.com>
2022-10-04 18:58:17 +0900

    getNenpyoObjData関数の動作テスト2 #3

--- pages/api/nenpyo/event/[title].ts
+++ pages/api/nenpyo/event/[title].ts
@@ -42,5 +42,5 @@ export default (req, res) => {
   console.log('nenpyoObjData', nenpyoObjData);
 
   // res.status(200).json(nenpyoObjData);
-  res.status(200).json({ title: title  });
+  res.status(200).json({ title: nenpyoObjData });
 }
