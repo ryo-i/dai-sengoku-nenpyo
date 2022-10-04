201a475e8140a351527bd45965797447b818cfc6
ryo-i <iidazz0109@gmail.com>
2022-10-05 07:20:48 +0900

    TrackBreadcrumb一部無効化 #3

--- components/TrackBreadcrumb.tsx
+++ components/TrackBreadcrumb.tsx
@@ -1,19 +1,19 @@
 import React, { useState, useContext }  from 'react';
 import Link from 'next/link';
 import { eventContext } from '../context/eventContext';
-import { trackContext } from '../context/trackContext';
+// import { trackContext } from '../context/trackContext';
 
 
 // Track Bread Crumb
 const TrackBreadcrumb = () => {
   // Hooks
   const {eventTitle, setEventTitle} = useContext(eventContext);
-  const {trackData, setTrackData} = useContext(trackContext);
+  // const {trackData, setTrackData} = useContext(trackContext);
 
   return (
     <ul className="breadcrumb">
       <li><Link href="/"><a>Home</a></Link></li>
-      <li><Link href={"/category/" + trackData.path}><a>{trackData.category}</a></Link></li>
+      {/* <li><Link href={"/category/" + trackData.path}><a>{trackData.category}</a></Link></li> */}
       <li>{eventTitle}</li>
     </ul>
   );
