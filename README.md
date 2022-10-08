fca73986fc465e3ccd811fa7a946356daeeba04a
ryo-i <iidazz0109@gmail.com>
2022-10-08 15:25:26 +0900

    adDate、waDate追加 #2

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 ## 進捗（コミット）
 
-- 年代、年間のテキスト一部削除 [#2](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2)
+- adDate、waDate追加 [#2](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2)
 - 出来事ページにパンくずリンク追加などリファクタ [#3](https://github.com/ryo-i/dai-sengoku-nenpyo/issues/3)
 
 ## 詳細（ブログ）
--- pages/about.tsx
+++ pages/about.tsx
@@ -58,7 +58,7 @@ function About() {
             <section>
                 <h2>進捗</h2>
                 <ul>
-                    <li>年代、年間のテキスト一部削除 <a href="https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2" target="_blank">#2</a></li>
+                    <li>adDate、waDate追加 <a href="https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2" target="_blank">#2</a></li>
                     <li>出来事ページにパンくずリンク追加などリファクタ <a href="https://github.com/ryo-i/dai-sengoku-nenpyo/issues/3" target="_blank">#3</a></li>
                 </ul>
             </section>
