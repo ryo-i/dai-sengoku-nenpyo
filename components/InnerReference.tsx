738eb0ea251fc0e11a310fcf91fead3a5c56c83c
ryo-i <iidazz0109@gmail.com>
2022-11-13 11:01:46 +0900

    参考文献に未着手の本も追加 #3

--- components/InnerReference.tsx
+++ components/InnerReference.tsx
@@ -16,7 +16,7 @@ function InnerReference() {
     return (
       <>
       <section>
-          <h3>歴史全般</h3>
+          <h2>歴史全般</h2>
           <ul>
             {reference.allHistory.map((data, index) =>
               <li  key={index}>
@@ -26,9 +26,49 @@ function InnerReference() {
           </ul>
         </section>
         <section>
-          <h3>戦国史</h3>
+          <h2>戦国史</h2>
           <ul>
             {reference.sengokuHistory.map((data, index) =>
+              <li  key={index}>
+                { data.author + '『' + data.title + '』' + data.publisher+ '(' + data.year + ')' + '【' + data.status + '】'}
+              </li>
+            )}
+          </ul>
+        </section>
+        <section>
+          <h2>地域史</h2>
+          <ul>
+            {reference.areaHistory.map((data, index) =>
+              <li  key={index}>
+                { data.author + '『' + data.title + '』' + data.publisher+ '(' + data.year + ')' + '【' + data.status + '】'}
+              </li>
+            )}
+          </ul>
+        </section>
+        <section>
+          <h2>合戦</h2>
+          <ul>
+            {reference.kassen.map((data, index) =>
+              <li  key={index}>
+                { data.author + '『' + data.title + '』' + data.publisher+ '(' + data.year + ')' + '【' + data.status + '】'}
+              </li>
+            )}
+          </ul>
+        </section>
+        <section>
+          <h2>人物</h2>
+          <ul>
+            {reference.jinbutsu.map((data, index) =>
+              <li  key={index}>
+                { data.author + '『' + data.title + '』' + data.publisher+ '(' + data.year + ')' + '【' + data.status + '】'}
+              </li>
+            )}
+          </ul>
+        </section>
+        <section>
+          <h2>その他</h2>
+          <ul>
+            {reference.etc.map((data, index) =>
               <li  key={index}>
                 { data.author + '『' + data.title + '』' + data.publisher+ '(' + data.year + ')' + '【' + data.status + '】'}
               </li>
