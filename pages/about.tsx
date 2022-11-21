17fbd2f9088db0d78d3098cf0d78cc23717a97dc
ryo-i <iidazz0109@gmail.com>
2022-11-21 20:47:46 +0900

    図解戦国史、年表1592年まで追加(内容入れ替え)  #2

--- pages/about.tsx
+++ pages/about.tsx
@@ -57,7 +57,7 @@ function About() {
             <section>
                 <h2>進捗</h2>
                 <ul>
-                    <li>図解戦国史、年表1592年まで追加 <a href="https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2" target="_blank">#2</a></li>
+                    <li>図解戦国史、年表1592年まで追加(内容入れ替え) <a href="https://github.com/ryo-i/dai-sengoku-nenpyo/issues/2" target="_blank">#2</a></li>
                     <li>参考文献追加 <a href="https://github.com/ryo-i/dai-sengoku-nenpyo/issues/3" target="_blank">#3</a></li>
                 </ul>
             </section>
